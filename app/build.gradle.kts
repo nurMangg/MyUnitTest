ee1776e145280d26d3f8861f231d411740c659d1
nurmangg <rohmanuyeoke@gmail.com>
2024-03-26 16:33:52 +0700

    Update: Melakukan Pengujian UI Test Menggunakan Espresso

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,4 +52,9 @@ dependencies {
 
     testImplementation("org.mockito:mockito-core:4.4.0")
     testImplementation("org.mockito:mockito-inline:4.4.0")
+
+    androidTestImplementation("androidx.test:runner:1.5.2")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test:rules:1.5.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
 }
